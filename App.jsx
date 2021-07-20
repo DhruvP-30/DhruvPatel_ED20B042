e981dacae6811da023b4c5a24ca6c2f5fcbfcfe6
DhruvP-30 <87657336+DhruvP-30@users.noreply.github.com>
2021-07-20 10:41:24 +0530

    create app.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Homey from './Components/title/title.jsx';
+import Navbar from './Components/navbar/navbar.jsx';
+
+function App() {
+  return (
+    <div >
+      <Homey/>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
